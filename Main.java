a24bb7c5c9673e5613e33ab14917fbba23d235a7
Joseph-2 <552890@my.cuhsd.org>
2022-12-08 09:48:50 -0800

    final project

--- Main.java
+++ Main.java
@@ -3,7 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
-        Object rect = new Object(12,10);
+
+        System.out.println("Enter length of your desired rectangle:");
+        int length = in.nextInt();
+
+        System.out.println("Enter width of your desired rectangle: ");
+        int width = in.nextInt();
+
+        Object rect = new Object(length,width);
         System.out.println(rect.drawRec());
         System.out.println(rect.tallWideRec(rect));
     }
